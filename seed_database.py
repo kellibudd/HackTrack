0fb5850f2c29bdfda4d7d6ce43ecb0349508d853
Kelli Budd <buddkelli1@gmail.com>
2020-06-08 16:25:47 -0500

    Add seed functions for teams, team_members, and comments

--- seed_database.py
+++ seed_database.py
@@ -16,16 +16,24 @@ def seed_users():
 
     for user in user_data:
 
-        firstname = user['athlete']['firstname']
-        lastname = user['athlete']['lastname']
-        phone = user['athlete']['phone']
-        email = user['athlete']['email']
-        password = user['athlete']['password']
-        prof_pic = user['athlete']['profile']
-        strava_id = user['athlete']['id']
-        strava_access_token = user['access_token']
-        strava_access_token_expir = user['expires_at']
-        strava_refresh_token = user['refresh_token']
+        firstname = user['firstname']
+        lastname = user['lastname']
+        phone = user['phone']
+        email = user['email']
+        password = user['password']
+
+        if not 'id' in user:
+            prof_pic = None
+            strava_id = None
+            strava_access_token = None
+            strava_access_token_expir = None
+            strava_refresh_token = None
+        else:
+            prof_pic = user['profile']
+            strava_id = user['id']
+            strava_access_token = user['access_token']
+            strava_access_token_expir = user['expires_at']
+            strava_refresh_token = user['refresh_token']
 
         crud.create_user(firstname, lastname, phone, email, password, prof_pic, 
                         strava_id, strava_access_token, strava_access_token_expir, 
@@ -39,6 +47,7 @@ def seed_activities():
     for activity in activity_data:
 
         user = User.query.filter(User.strava_id==activity['athlete']['id']).first()
+        user_id = user.id
         strava_activity_id = activity['id']
         date_utc = activity['start_date']
         desc = activity['name']
@@ -61,22 +70,57 @@ def seed_activities():
 
         elev_gain = activity['total_elevation_gain']
 
-        print(user)
-        print(strava_activity_id)
-        print(date_utc)
-        print(desc)
-        print(exercise_type)
-        print(distance)
-        print(time_length)
-        print(average_speed)
-        print(max_speed)
-        print(has_heartrate)
-        print(effort) 
-        print(effort_source)
-        print(elev_gain)
-        print('*'*20)
-
-        crud.create_activity(user, strava_activity_id, date_utc, 
+        crud.create_activity(user_id, strava_activity_id, date_utc, 
                     desc, exercise_type, distance, time_length,
                     average_speed, max_speed, has_heartrate, effort, 
                     effort_source, elev_gain)
+
+def seed_teams():
+
+    with open('data/teams.json') as f:
+        team_data = json.loads(f.read())
+
+    for team in team_data:
+
+        name = team['name']
+        coach_id = team['coach_id']
+        logo = team['logo']
+        team_banner_img = team['team_banner_img']
+        team_color = team['team_color']
+
+        crud.create_team(name, coach_id, logo, team_banner_img, team_color)
+
+def seed_team_members():
+
+    with open('data/team_members.json') as f:
+        team_member_data = json.loads(f.read())
+
+    for member in team_member_data:
+
+        user_id = member['user_id']
+        team_id = member['team_id']
+        role = member['role']
+
+        crud.create_team_member(user_id, team_id, role)
+
+def seed_comments():
+
+    with open('data/comments.json') as f:
+        comment_data = json.loads(f.read())
+
+    for comment in comment_data:
+
+        activity_id = comment['activity_id']
+        author_id = comment['author_id']
+        date_utc = comment['date_utc']
+        body = comment['body']
+
+        crud.create_comment(activity_id, author_id, date_utc, body)
+
+seed_users()
+seed_activities()
+seed_teams()
+seed_team_members()
+seed_comments()
+
+
