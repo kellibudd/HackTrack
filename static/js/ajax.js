7ee4014e71a574a919cdce43cd2c8e677c9d61e5
Kelli Budd <buddkelli1@gmail.com>
2020-06-12 14:25:42 -0500

    Update id name

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -2,7 +2,7 @@
 
 
 $( document ).ready(function() {
-    $('#registration-form').on('submit', (evt) => {
+    $('#user-reg-form').on('submit', (evt) => {
         if ($('#password-field').val() !== $('#password-confirm-field').val()) {
             evt.preventDefault()
             alert("Password fields do not match. Please try again.");
