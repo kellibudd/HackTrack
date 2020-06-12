df80040e0b0764a0823cdb984386595437679b8a
Kelli Budd <buddkelli1@gmail.com>
2020-06-11 19:58:26 -0500

    Remove hello world console log

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -2,11 +2,10 @@
 
 
 $( document ).ready(function() {
-    console.log('hello world')
     $('#registration-form').on('submit', (evt) => {
         if ($('#password-field').val() !== $('#password-confirm-field').val()) {
             evt.preventDefault()
             alert("Password fields do not match. Please try again.");
         };
-    }); 
+    });
 });
