8bb33c4724276d5f228f0295dd9bdb21e3b28969
Kelli Budd <buddkelli1@gmail.com>
2020-06-17 16:05:02 -0500

    Update function to pull new activities and load into database

--- crud.py
+++ crud.py
@@ -1,6 +1,7 @@
 """CRUD operations."""
 
 from model import connect_to_db, db, User, Activity, Team, Team_Member, Comment
+import requests
 from datetime import datetime, timedelta
 from pytz import timezone
 import strava_api
@@ -52,6 +53,46 @@ def create_activity(user_id, strava_activity_id, date_utc, date_local,
     db.session.commit()
 
     return activity
+
+def create_activity_from_strava_data(activity):
+
+    user = User.query.filter(User.strava_id==activity['athlete']['id']).first()
+    user_id = user.id
+    strava_activity_id = activity['id']
+    date_utc = activity['start_date']
+    date_local = activity['start_date_local']
+    desc = activity['name']
+    exercise_type = activity['type']
+    distance = round(activity['distance'] * 0.000621371, 2)
+
+    workout_time = activity['moving_time']
+
+    if distance < 0:
+        avg_time = (activity['moving_time'] / 60) / distance
+        avg_minutes = int(avg_time)
+        avg_seconds = round((avg_time % 1) * 60)
+        average_speed = f'{avg_minutes}:{avg_seconds}/mile'
+    else:
+        average_speed = 'N/A'
+
+    has_heartrate = activity['has_heartrate']
+
+    if activity['has_heartrate']:
+        effort_source = 'heartrate'
+    else:
+        effort_source = 'perceived exertion'
+
+    if not 'suffer_score' in activity:
+        effort = 0
+    else:
+        effort = activity['suffer_score']
+
+    elev_gain = activity['total_elevation_gain']
+
+    create_activity(user_id, strava_activity_id, date_utc, date_local, 
+                desc, exercise_type, distance, workout_time,
+                average_speed, has_heartrate, effort, 
+                effort_source, elev_gain)
 
 # def create_activity_with_strava_data(activity):
 
@@ -185,6 +226,10 @@ def update_access_tokens(team_id):
         athlete.strava_access_token = token['access_token']
         db.session.commit()
 
+    updated_athlete_data = get_all_athlete_data_by_team(team_id)
+
+    return updated_athlete_data
+
 def get_strava_activity_ids(team_id):
 
     athlete_ids = get_athlete_ids_by_team(team_id)
@@ -199,17 +244,23 @@ def get_strava_activity_ids(team_id):
     return strava_activity_ids
 
 
-# def update_db_activities(team_id):
+def update_db_activities(team_id):
 
-#     athletes = get_all_athlete_data_by_team(team_id)
+    athletes = update_access_tokens(team_id)
 
-#     for athlete in athletes:
-#         access_token = athlete.strava_access_token
-#         header = {'Authorization': 'Bearer ' + access_token}                                                                      
-#         activities_data = requests.get(activities_url, headers=header).json()
+    for athlete in athletes:
+        access_token = athlete.strava_access_token
+        header = {'Authorization': 'Bearer ' + access_token}
+        activities_url = 'https://www.strava.com/api/v3/athlete/activities'                                                                      
+        activities_data = requests.get(activities_url, headers=header).json()
+        print(athlete.firstname)
+        print('*'*20)
+        print(activities_data)
+        print(type(activities_data))
 
-#         for activity in activities_data:
-#             if activity.id not in get_strava_activity_ids(team_id):
+        for activity in activities_data:
+            if not activity['id'] in get_strava_activity_ids(team_id):
+                create_activity_from_strava_data(activity)
 
 
 # def get_activities_by_team(team_id):
@@ -260,7 +311,7 @@ def convert_time_format(time):
 def get_current_week_activities(team_id, user_timezone):
     """Return a list of activities completed by users on a team during the current week."""
 
-    today = datetime.utcnow().astimezone(timezone(user_timezone)) - timedelta(days=2)
+    today = datetime.utcnow().astimezone(timezone(user_timezone))
     today = datetime(year=today.year, month=today.month,
                     day=today.day, hour=23, minute= 59, second=59)
     print("today is", today)
@@ -274,15 +325,16 @@ def get_current_week_activities(team_id, user_timezone):
 
     weekdays = {0: 'monday', 1: 'tuesday', 2: 'wednesday', 3: 'thursday', 4: 'friday', 5: 'saturday', 6: 'sunday'}
 
+    curr_week_activities = {}
 
     for athlete_id in athlete_ids:
-        curr_week_activities[athlete_id] = {'monday': {'Run':{'distance': '-'}, 'Cross Train':{'workout_time': '-'}},
-                                        'tuesday': {'Run':{'distance': '-'}, 'Cross Train':{'workout_time': '-'}},
-                                        'wednesday': {'Run':{'distance': '-'}, 'Cross Train':{'workout_time': '-'}},
-                                        'thursday': {'Run':{'distance': '-'}, 'Cross Train':{'workout_time': '-'}},
-                                        'friday': {'Run':{'distance': '-'}, 'Cross Train':{'workout_time': '-'}},
-                                        'saturday': {'Run':{'distance': '-'}, 'Cross Train':{'workout_time': '-'}},
-                                        'sunday': {'Run':{'distance': '-'}, 'Cross Train':{'workout_time': '-'}},
+        curr_week_activities[athlete_id] = {'monday': {'Run':{'distance': '·'}, 'Cross Train':{'workout_time': '·'}},
+                                        'tuesday': {'Run':{'distance': '·'}, 'Cross Train':{'workout_time': '·'}},
+                                        'wednesday': {'Run':{'distance': '·'}, 'Cross Train':{'workout_time': '·'}},
+                                        'thursday': {'Run':{'distance': '·'}, 'Cross Train':{'workout_time': '·'}},
+                                        'friday': {'Run':{'distance': '·'}, 'Cross Train':{'workout_time': '·'}},
+                                        'saturday': {'Run':{'distance': '·'}, 'Cross Train':{'workout_time': '·'}},
+                                        'sunday': {'Run':{'distance': '·'}, 'Cross Train':{'workout_time': '·'}},
                                         'total_mileage': 0,
                                         'total_xtrain_mins': 0}
 
@@ -315,7 +367,7 @@ def get_current_week_activities(team_id, user_timezone):
                                                                                                             "elev_gain" : activity.elev_gain}
     for athlete_id in athlete_ids:
         if curr_week_activities[athlete_id]['total_xtrain_mins'] == 0:
-            curr_week_activities[athlete_id]['total_xtrain_mins'] = "-"
+            curr_week_activities[athlete_id]['total_xtrain_mins'] = ""
         else:
             curr_week_activities[athlete_id]['total_xtrain_mins'] = convert_time_format(curr_week_activities[athlete_id]['total_xtrain_mins'])
 
