35769e3454267ecf70dbede59709dff9292efeb1
Kelli Budd <buddkelli1@gmail.com>
2020-06-30 23:25:10 -0500

    Comment out seed activities and comments

--- seed_database.py
+++ seed_database.py
@@ -22,7 +22,6 @@ def seed_users():
         phone = user['phone']
         email = user['email']
         password = user['password']
-        timezone = user['timezone']
 
         if not 'id' in user:
             prof_pic = None
@@ -37,34 +36,34 @@ def seed_users():
             strava_access_token_expir = user['expires_at']
             strava_refresh_token = user['refresh_token']
 
-        crud.create_user(firstname, lastname, phone, email, password, prof_pic, timezone, 
+        crud.create_user(firstname, lastname, phone, email, password, prof_pic, 
                         strava_id, strava_access_token, strava_access_token_expir, 
                         strava_refresh_token)
 
-def seed_activities():
+# def seed_activities():
 
-    with open('data/activities.json') as f:
-        activity_data = json.loads(f.read())
+#     with open('data/activities.json') as f:
+#         activity_data = json.loads(f.read())
 
-    for activity in activity_data:
-        athlete = User.query.filter(User.strava_id==activity['athlete']['id']).first()
+#     for activity in activity_data:
+#         athlete = User.query.filter(User.strava_id==activity['athlete']['id']).first()
 
-        date = datetime.strptime(activity['start_date_local'].split('T')[0], '%Y-%m-%d')
+#         date = datetime.strptime(activity['start_date_local'].split('T')[0], '%Y-%m-%d')
         
-        new_activity = Activity(user_id=athlete.id,
-                        strava_activity_id=activity['id'],
-                        date_utc=activity['start_date'],
-                        date_local=activity['start_date_local'],
-                        week_num=date.isocalendar()[1],
-                        weekday=date.isocalendar()[2],
-                        desc=activity['name'],
-                        exercise_type=activity['type'],
-                        distance=activity['distance'],
-                        workout_time=activity['moving_time'],
-                        elev_gain=activity['total_elevation_gain'])
+#         new_activity = Activity(user_id=athlete.id,
+#                         strava_activity_id=activity['id'],
+#                         date_utc=activity['start_date'],
+#                         date_local=activity['start_date_local'],
+#                         week_num=date.isocalendar()[1],
+#                         weekday=date.isocalendar()[2],
+#                         desc=activity['name'],
+#                         exercise_type=activity['type'],
+#                         distance=activity['distance'],
+#                         workout_time=activity['moving_time'],
+#                         elev_gain=activity['total_elevation_gain'])
 
-        db.session.add(new_activity)
-        db.session.commit()
+#         db.session.add(new_activity)
+#         db.session.commit()
 
 def seed_teams():
 
@@ -73,14 +72,15 @@ def seed_teams():
 
     for team in team_data:
 
-        name = team['name']
-        coach_id = team['coach_id']
-        logo = team['logo']
-        team_banner_img = team['team_banner_img']
-        team_color = team['team_color']
-        activities_last_updated = team['activities_last_updated']
+        team = Team(name=team['name'], 
+                    coach_id=team['coach_id'],
+                    logo=team['logo'],
+                    team_banner_img=team['team_banner_img'],
+                    team_color=team['team_color'],
+                    activities_last_updated=team['activities_last_updated'])
 
-        crud.create_team(name, coach_id, logo, team_banner_img, team_color, activities_last_updated)
+        db.session.add(team)
+        db.session.commit()
 
 def seed_team_members():
 
@@ -95,25 +95,25 @@ def seed_team_members():
 
         crud.create_team_member(user_id, team_id, role)
 
-def seed_comments():
+# def seed_comments():
 
-    with open('data/comments.json') as f:
-        comment_data = json.loads(f.read())
+#     with open('data/comments.json') as f:
+#         comment_data = json.loads(f.read())
 
-    for comment in comment_data:
+#     for comment in comment_data:
 
-        activity_id = comment['activity_id']
-        author_id = comment['author_id']
-        recipient_id = comment['recipient_id']
-        date_utc = comment['date_utc']
-        body = comment['body']
+#         activity_id = comment['activity_id']
+#         author_id = comment['author_id']
+#         recipient_id = comment['recipient_id']
+#         date_utc = comment['date_utc']
+#         body = comment['body']
 
-        crud.create_comment(activity_id, author_id, recipient_id, date_utc, body)
+#         crud.create_comment(activity_id, author_id, recipient_id, date_utc, body)
 
 seed_users()
-seed_activities()
+# seed_activities()
 seed_teams()
 seed_team_members()
-seed_comments()
+# seed_comments()
 
 
